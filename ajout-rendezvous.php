dda42dca5c9c4b46b74ada66b90b315a0d30054c
moonye@live.fr <moonye@live.fr>
2024-12-10 16:50:25 +0100

    EXO 6

--- ajout-rendezvous.php
+++ ajout-rendezvous.php
@@ -2,7 +2,7 @@
 require_once './utils/connect_db.php';
 
 
-$sql = "SELECT patients.lastname, patients.firstname, patients.birthdate, appointments.dateHour, appointments.dateHour 
+$sql = "SELECT patients.lastname, patients.firstname, patients.birthdate, appointments.dateHour, patients.id, appointments.dateHour 
 FROM patients 
 LEFT JOIN appointments ON appointments.idPatients = patients.id;";
 
@@ -15,8 +15,6 @@ try {
     echo "Erreur lors de la requête : " . $error->getMessage();
     exit;
 }
-
-var_dump($users);
 
 ?>
 
@@ -32,25 +30,21 @@ var_dump($users);
 </head>
 
 <body>
-<?php
-
-foreach ($users as $user) {
-
-$nomPrenomPatient = ($user['lastname']) . ' ' . ($user['firstname']);
-
-
-?>
-
-
 
     <h1> Prise de rendez-vous </h1>
     <form action="./process/process_create_rdv.php" method="post">
 
-        <select name="find-patient" id="find-patient">
-            <option value="<? $nomPrenomPatient ?>">.</option>
-        </select>
+   <label for="find-patient">Nom prénom du patient :</label>
+   <select name="id" id="find-patient">
+     <?php foreach ($users as $user): ?>
+        <option value="<?= htmlspecialchars($user['id']); ?>"> <!-- La valeur de ce qui sera afficher correspond à l'ID -->
+            <?= htmlspecialchars($user['lastname']) . ' ' . htmlspecialchars($user['firstname']); ?> <!-- Ce qui est affiché  c'est le nom et prénom mais n'a pas de valeur -->
+        </option>
+    <?php endforeach; ?>
+</select>
 
-        <label for="dateHour">Date de naissance :</label>
+
+        <label for="dateHour">Date du rendez-vous :</label>
         <input type="datetime-local" name="dateHour" id="dateHour" required>
 
 
@@ -66,9 +60,5 @@ $nomPrenomPatient = ($user['lastname']) . ' ' . ($user['firstname']);
         <input type="submit" value="Prendre rendez-vous">
     </form>
 </body>
-<?php
-        }
-    
-        ?>
 
 </html>
--- process/process_create_rdv.php
+++ process/process_create_rdv.php
@@ -1,45 +1,34 @@
 <?php
-
 require_once '../utils/connect_db.php';
 
-if (
-    empty($_POST['lastName']) || empty($_POST['firstName']) || empty($_POST['birthdate']) ||
-    empty($_POST['consultation'])|| empty($_POST['daterdv'])
-) {
+
+// var_dump($_POST);
+// exit;
+
+
+// Vérification que tous les champs sont remplis
+if (empty($_POST['id']) || empty($_POST['dateHour'])) {
     echo "Tous les champs ne sont pas remplis !";
     echo "<br>";
     echo '<a href="../ajout-rendezvous.php" class="btn">Retour</a>';
     exit;
-
 }
 
-
-
-
-
-$sql = "INSERT INTO appointments (lastName, firstName, birthdate, consultation, daterdv)
- VALUES (:lastName, :firstName , :birthdate, :consultation, daterdv)";
+// Préparation de la requête SQL
+$sql = "INSERT INTO appointments (idPatients, dateHour) 
+        VALUES (:idPatients, :dateHour)";
 
 try {
     $stmt = $pdo->prepare($sql);
-    $users = $stmt->execute([
-        ':lastName' => $_POST["firstName"],
-        ':firstname' => $_POST["firstname"],
-        ':birthdate' => $_POST["birthdate"],
-        ':consultation' => $_POST["consultation"],
-        ':daterdv' => $_POST["daterdv"]
-
-
+    $stmt->execute([
+        ':idPatients' => $_POST['id'],       
+        ':dateHour' => $_POST['dateHour']   
     ]);
-     // ou fetch si vous savez que vous n'allez avoir qu'un seul résultat
 
-echo "Votre rendez-vous à été confirmé !";
-echo "<br>";
-echo ' Revenir à la page d\'accueil :<a href="../index.php" class="btn">Page d\'accueil</a>';
-
+    echo "Votre rendez-vous a été confirmé !";
+    echo "<br>";
+    echo 'Revenir à la page d\'accueil : <a href="../index.php" class="btn">Page d\'accueil</a>';
 
 } catch (PDOException $error) {
-    echo "Erreur lors de la requete : " . $error->getMessage();
+    echo "Erreur lors de la requête : " . $error->getMessage();
 }
-
-
